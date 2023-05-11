0794b52c279ac20f7b03dae4c35b19acec9021b4
RaghadAlhuzali <120299765+RaghadAlhuzali@users.noreply.github.com>
2023-05-11 13:36:15 +0000

    raghad

--- src/main/java/edu/uqu/cs/Home.java
+++ src/main/java/edu/uqu/cs/Home.java
@@ -1,41 +1,79 @@
-package edu.uqu.cs;
-/*
- * Lab 2 
- * CS 1312
- */
+public class Home {
 
-/*
-* Make sure to complete and submit your lab
-*/
-import java.util.Scanner;
+    public static void main(String[] args) {
 
-public class Home{
-	public static void main(String [] args){
+        /*
+         * create an object of class Room
+         */ 
+         Room R = new Room();
+        /*
+         * set the instance variable room number's value to 101
+         */ 
+        R.setRoomNumber(101);
+        /*
+         * print the instance variable room number
+         */ 
+        System.out.println(R.getRoomNumber());
+        /*
+         * add light to the room with 
+         * an id= 100, and type= "Philips"
+         * by invoking method addLight (id, type);
+         */ 
+        R.addLight(100, "Philips");
+        /*
+         * turn light with id equals to 100 on
+         * by invoking method turnLightOn (id);
+         */ 
+        R.turnLightON(100);
+        /*
+         * add light to the room with 
+         * an id= 200, and type= "Philips"
+         * by invoking method addLight (id, type);
+         */ 
+        R.addLight(200, "Philips");
 
-		// Create an object of type Garage 
-                //HERE
+        /*
+         * Check if the light with id 100 is on or off in the room
+         * Hint: call method lightStatus (id) of class Room
+         */ 
+       if( R.lightStatus(100))
+       {
+        System.out.println("Light is on");
+       }
+        /*
+         * switch light with id 100 OFF
+         * Hint: call method turnLightOFF (id) of class Room
+         */
+     R.turnLightOFF(100);
+        /*
+         * switch light with id 100 ON
+         * Hint: call method turnLightON (id) of class Room
+         */
+        R.turnLightON(100);
 
-
-	        // Add "Toyota Corolla Cross" in the garage. Hint: use method addCar () of class Garage
-                //HERE
-
-                //List all the cars in the garage. Hint: use method listCars () of class Garage
-                //HERE
-
-                // Place "Ford Fusion" in the garage. Hint: use method addCar () of class Garage
-                //HERE
-
- 	        // List all the cars in the garage once again. Hint: use method listCars () of class Garage
-                //HERE
-
-
-               // Move out "Ford Fusion" from the garage. Hint: use method moveOut () of class Garage
-               //HERE
-
-
-               // List all the cars in the garage. Hint: use method listCars () of class Garage
-               //HERE
-
-        }
+        /*
+         * Check if the door is locked or not in a given room
+         * Hint: call method doorStatus () of class Room
+         */ 
+        if( R.doorStatus())
+       {
+        System.out.println("Door is Locked");
+       }else{
+        
+            System.out.println("Door is Opened");
+           
+       }
+        /*
+         * Unlock the door that is in a given room
+         * Hint: call method unlockDoor() of class Room
+         */ 
+        R.unlockDoor();
+        /*
+         * Display all lights and door status in a given room
+         * Hint: call method printRoomstatus() of class Room
+         */ 
+       R.printRoomStatus();
+    
+    }
 
 }
